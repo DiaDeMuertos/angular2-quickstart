91640be98e2ad5ed5dd15810f8457f4718fccc1f
DiaDeMuertos <imworkingnow@msn.com>
2016-09-04 18:54:23 -0700

    cud

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService }  from './models/in-memory-data.service';
 
 import { AppComponent }  from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -10,10 +14,10 @@ import { HeroService } from './services/hero.service';
 import { routing } from './app.routing';
 
 @NgModule({
-  bootstrap: [ AppComponent ],
+  imports: [ BrowserModule, FormsModule, routing, HttpModule, InMemoryWebApiModule.forRoot(InMemoryDataService)],
   declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent ],
-  imports: [ BrowserModule, FormsModule, routing],
   providers: [ HeroService ],
+  bootstrap: [ AppComponent ],
 })
 
 export class AppModule { }
